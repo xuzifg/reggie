758c6c42e60cca6232d741c0a9798fbaa469393d
anjianing <129052046+Ankh1221@users.noreply.github.com>
2023-06-15 21:36:19 +0800

    Update BaseContext.java

--- src/main/java/edu/czjt/reggie/common/BaseContext.java
+++ src/main/java/edu/czjt/reggie/common/BaseContext.java
@@ -2,10 +2,11 @@ package edu.czjt.reggie.common;
 
 //create by jianing.an
 public class BaseContext {
-
+//包含一个静态成员变量threadLocal,维护了一个与当前线程相关联的 Long 类型的值。
+    
     private static ThreadLocal<Long> threadLocal = new ThreadLocal<>();
   
-    //设置值 : param id
+    //setCurrentID:用于将当前线程的 threadLocal 对象的值设置为传入的 id 参数。
     public static void setCurrentId(Long id){
         threadLocal.set(id);
     }
@@ -14,6 +15,7 @@ public class BaseContext {
      * 获取值
      * @return
      */
+    //getCurrentId:用于获取当前线程的 threadLocal 对象的值。
     public static Long getCurrentId(){
         return threadLocal.get();
     }
