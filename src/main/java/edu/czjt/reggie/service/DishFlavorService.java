18175d4afe2aec0238068b9875cf4664c35bf88a
Jesse-qi <130468473+Jesse-qi@users.noreply.github.com>
2023-06-15 21:45:49 +0800

    Update DishFlavorService.java

--- src/main/java/edu/czjt/reggie/service/DishFlavorService.java
+++ src/main/java/edu/czjt/reggie/service/DishFlavorService.java
@@ -1,1 +1,17 @@
+package edu.czjt.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import edu.czjt.reggie.entity.DishFlavor;
+
+import java.util.List;
+
+/**
+ * 齐龙玉
+ */
+public interface DishFlavorService extends IService<DishFlavor> {
+
+    public List<DishFlavor> getFlavorsByDishId(Long dishId);
+
+    boolean removeByDishId(Long id);
+}
 
