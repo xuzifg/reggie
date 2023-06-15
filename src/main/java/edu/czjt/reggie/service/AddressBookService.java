d03d4779a14e98241918316d727cba0622b6b63f
3520272871 <136344413+3520272871@users.noreply.github.com>
2023-06-15 21:41:02 +0800

    Update AddressBookService.java

--- src/main/java/edu/czjt/reggie/service/AddressBookService.java
+++ src/main/java/edu/czjt/reggie/service/AddressBookService.java
@@ -1,8 +1,1 @@
-package edu.czjt.reggie.service;
 
-import com.baomidou.mybatisplus.extension.service.IService;
-import edu.czjt.reggie.entity.AddressBook;
-
-public interface AddressBookService extends IService<AddressBook> {
-
-}
