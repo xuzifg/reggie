c765ad3fd77f01c953cda19f731027b51fcc8929
anjianing <129052046+Ankh1221@users.noreply.github.com>
2023-06-15 22:28:32 +0800

    Update R.java

--- src/main/java/edu/czjt/reggie/common/R.java
+++ src/main/java/edu/czjt/reggie/common/R.java
@@ -6,19 +6,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Created by jinkun.tian on 2023/3/21
+ * Created by jianing.an
  */
 @Data
+//统一的返回结果类，用于封装请求的响应结果。
 public class R<T> {
 
-    private Integer code; //编码：1成功，0和其它数字为失败
+    private Integer code; //请求响应码，1 表示成功，0 或其它数字表示失败
 
-    private String msg; //错误信息
+    private String msg; //错误信息，请求响应的消息文本
 
-    private T data; //数据
+    private T data; //请求响应的数据
 
-    private Map map = new HashMap(); //动态数据
+    private Map map = new HashMap(); //动态数据，在某些场景下，我们需要在接口返回的结果中加入一些动态数据，比如记录操作结果的日志等。
 
+    //静态方法：success：请求成功时使用，它接收一个泛型类型的 object 参数，表示请求成功时返回的数据
     public static <T> R<T> success(T object) {
         R<T> r = new R<T>();
         r.data = object;
@@ -26,6 +28,7 @@ public class R<T> {
         return r;
     }
 
+    //error：请求失败时使用，它接收一个字符串类型的 msg 参数，表示请求失败时的错误信息。
     public static <T> R<T> error(String msg) {
         R r = new R();
         r.msg = msg;
@@ -33,6 +36,7 @@ public class R<T> {
         return r;
     }
 
+    //add：用于向 map 中添加动态数据，它接受一个字符串类型的 key 和一个 value 参数，并返回当前 R 对象本身，这样就可以进行链式调用。
     public R<T> add(String key, Object value) {
         this.map.put(key, value);
         return this;
