64db803be8360fe3b14e457fc801c825c3d0665a
czt2001 <136036274+czt2001@users.noreply.github.com>
2023-06-15 20:54:12 +0800

    Update Category.java

--- src/main/java/edu/czjt/reggie/entity/Category.java
+++ src/main/java/edu/czjt/reggie/entity/Category.java
@@ -1,53 +1,102 @@
-package edu.czjt.reggie.entity;
+package edu.czjt.reggie.controller;
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.TableField;
-import lombok.Data;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import edu.czjt.reggie.common.R;
+import edu.czjt.reggie.entity.Category;
+import edu.czjt.reggie.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import java.io.Serializable;
-import java.time.LocalDateTime;
+import java.util.List;
 
 /**
- * 分类
+ * 分类管理
  */
 //陈智拓做
-@Data
-public class Category implements Serializable {
+@RestController
+@RequestMapping("/category")
+@Slf4j
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
 
-    private static final long serialVersionUID = 1L;
+    /**
+     * 新增分类
+     * @param category
+     * @return
+     */
+    @PostMapping
+    public R<String> save(@RequestBody Category category){
+        log.info("category:{}",category);
+        categoryService.save(category);
+        return R.success("新增分类成功");
+    }
 
-    private Long id;
+    /**
+     * 分页查询
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page,int pageSize){
+        //分页构造器
+        Page<Category> pageInfo = new Page<>(page,pageSize);
+        //条件构造器
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+        //添加排序条件，根据sort进行排序
+        queryWrapper.orderByAsc(Category::getSort);
 
+        //分页查询
+        categoryService.page(pageInfo,queryWrapper);
+        return R.success(pageInfo);
+    }
 
-    //类型 1 菜品分类 2 套餐分类
-    private Integer type;
+    /**
+     * 根据id删除分类
+     * @param id
+     * @return
+     */
+    @DeleteMapping
+    public R<String> delete(Long id){
+        log.info("删除分类，id为：{}",id);
 
+        categoryService.removeById(id);
 
-    //分类名称
-    private String name;
+        return R.success("分类信息删除成功");
+    }
 
+    /**
+     * 根据id修改分类信息
+     * @param category
+     * @return
+     */
+    @PutMapping
+    public R<String> update(@RequestBody Category category){
+        log.info("修改分类信息：{}",category);
 
-    //顺序
-    private Integer sort;
+        categoryService.updateById(category);
 
+        return R.success("修改分类信息成功");
+    }
 
-    //创建时间
-    @TableField(fill = FieldFill.INSERT)
-    private LocalDateTime createTime;
+    /**
+     * 根据条件查询分类数据
+     * @param category
+     * @return
+     */
+    @GetMapping("/list")
+    public R<List<Category>> list(Category category){
+        //条件构造器
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+        //添加条件
+        queryWrapper.eq(category.getType() != null,Category::getType,category.getType());
+        //添加排序条件
+        queryWrapper.orderByAsc(Category::getSort).orderByDesc(Category::getUpdateTime);
 
-
-    //更新时间
-    @TableField(fill = FieldFill.INSERT_UPDATE)
-    private LocalDateTime updateTime;
-
-
-    //创建人
-    @TableField(fill = FieldFill.INSERT)
-    private Long createUser;
-
-
-    //修改人
-    @TableField(fill = FieldFill.INSERT_UPDATE)
-    private Long updateUser;
-
+        List<Category> list = categoryService.list(queryWrapper);
+        return R.success(list);
+    }
 }
