065b44591b8dc9df4d7e7e0b0f0525f473921f93
Jesse-qi <130468473+Jesse-qi@users.noreply.github.com>
2023-06-15 21:43:56 +0800

    Update DishFlavorMapper.java

--- src/main/java/edu/czjt/reggie/mapper/DishFlavorMapper.java
+++ src/main/java/edu/czjt/reggie/mapper/DishFlavorMapper.java
@@ -1,1 +1,13 @@
+package edu.czjt.reggie.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import edu.czjt.reggie.entity.DishFlavor;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 齐龙玉
+ */
+@Mapper
+public interface DishFlavorMapper extends BaseMapper<DishFlavor> {
+}
 
