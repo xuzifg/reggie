872d8eb1684382798387de501a2ea0b3839d432d
Jesse-qi <130468473+Jesse-qi@users.noreply.github.com>
2023-06-15 21:38:16 +0800

    Update DishDto.java

--- src/main/java/edu/czjt/reggie/dto/DishDto.java
+++ src/main/java/edu/czjt/reggie/dto/DishDto.java
@@ -1,1 +1,21 @@
+package edu.czjt.reggie.dto;
+
+import edu.czjt.reggie.entity.Dish;
+import edu.czjt.reggie.entity.DishFlavor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 创建者 齐龙玉 
+ */
+@Data
+public class DishDto extends Dish {
+
+    private List<DishFlavor> flavors = new ArrayList<>();
+
+    private String categoryName;
+
+}
 
