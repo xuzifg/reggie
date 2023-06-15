0084a9e805ad70ccd1b44991a17aa602a8503478
jiaxuxuxuxu <136545829+jiaxuxuxuxu@users.noreply.github.com>
2023-06-15 21:52:27 +0800

    Update OrderDetailService.java

--- src/main/java/edu/czjt/reggie/service/OrderDetailService.java
+++ src/main/java/edu/czjt/reggie/service/OrderDetailService.java
@@ -1,8 +1,1 @@
-package edu.czjt.reggie.service;
 
-import com.baomidou.mybatisplus.extension.service.IService;
-import edu.czjt.reggie.entity.OrderDetail;
-
-public interface OrderDetailService extends IService<OrderDetail> {
-
-}
