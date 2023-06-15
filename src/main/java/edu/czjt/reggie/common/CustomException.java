6a1130cbdbb42f0eeb47d144e8ba997a39b4c8f2
anjianing <129052046+Ankh1221@users.noreply.github.com>
2023-06-15 21:54:26 +0800

    Update CustomException.java

--- src/main/java/edu/czjt/reggie/common/CustomException.java
+++ src/main/java/edu/czjt/reggie/common/CustomException.java
@@ -1,10 +1,12 @@
 package edu.czjt.reggie.common;
 
-
- // 自定义业务异常类
+//create by jianing.an
+ // 自定义业务异常类，用于封装应用程序的业务异常。
  
 public class CustomException extends RuntimeException {
+ //构造函数中传递一个字符串类型的 message 参数，该异常会使用此参数初始化其父类 RuntimeException 的消息文本。
     public CustomException(String message){
+     //在子类的构造函数中调用父类的构造函数，并传递子类的参数。
         super(message);
     }
 }
