ca916c5462017ed812d11ff2a3bf7815cbf865c7
xuzifg <135988870+xuzifg@users.noreply.github.com>
2023-06-16 08:35:59 +0800

    Update ShoppingCartMapper.java

--- src/main/java/edu/czjt/reggie/mapper/ShoppingCartMapper.java
+++ src/main/java/edu/czjt/reggie/mapper/ShoppingCartMapper.java
@@ -1,5 +1,5 @@
 package edu.czjt.reggie.mapper;
-
+// 购物车接口，许子枫
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import edu.czjt.reggie.entity.ShoppingCart;
 import org.apache.ibatis.annotations.Mapper;
